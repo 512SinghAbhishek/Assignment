3204e468cf2c5539b957144aeae787d6dfec54eb
bookmyappointment404 <90716652+512SinghAbhishek@users.noreply.github.com>
2023-12-22 10:48:18 +0530

    first commit

---
+++ src/Component/Test.jsx
@@ -0,0 +1,256 @@
+import React from 'react'
+
+function Test() {
+    return (
+        <div>
+            <div class="landing-page">
+                <img class="landing-page-child" alt="" src="image/mdi_truck-fast-outline.svg" />
+
+                <div class="landing-page-inner">
+                    <div class="group-parent">
+                        <div class="rectangle-parent">
+                            <div class="group-child"></div>
+                            <div class="full-name-parent">
+                                <div class="full-name">full name</div>
+                                <div class="frame-wrapper">
+                                    {/* <input className='form-control' type='text' ></input> */}
+                                    <div class="iconlyboldlocation-parent">
+                                        <img
+                                            class="iconlyboldlocation"
+                                            alt=""
+                                            src="image/Location.png"
+                                        />
+
+                                        <div type='text' class="enter-location" pla>enter location</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="origin-wrapper">
+                                <div class="origin">origin</div>
+                            </div>
+                        </div>
+                        <div class="rectangle-group">
+                            <div class="group-item"></div>
+                            <div class="full-name-parent">
+                                <div class="full-name">full name</div>
+                                <div class="frame-wrapper">
+                                    <div class="iconlyboldlocation-parent">
+                                        <img
+                                            class="iconlyboldlocation"
+                                            alt=""
+                                            src="image/Location.png"
+                                        />
+
+                                        <div class="enter-location">enter location</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="origin-wrapper">
+                                <div class="origin">desitination</div>
+                            </div>
+                        </div>
+                        <div class="rectangle-group">
+                            <div class="group-inner"></div>
+                            <div class="full-name-parent">
+                                <div class="full-name">full name</div>
+                                <div class="frame-wrapper">
+                                    <div class="iconlyboldlocation-parent">
+                                        <img
+                                            class="iconlyboldlocation"
+                                            alt=""
+                                            src="image/Bag.png"
+                                        />
+
+                                        <div class="enter-location">weight (KG0</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="origin-wrapper">
+                                <div class="origin">weight</div>
+                            </div>
+                        </div>
+                        <button class="check-price-wrapper">
+                            <div class="check-price">check price</div>
+                        </button>
+                    </div>
+                </div>
+                <div class="frame-parent">
+                    <img class="frame-icon" alt="" src="image/bx_bxs-plane-take-off.svg" />
+
+                    <div class="rectangle-parent1">
+                        <div class="frame-child"></div>
+                        <div class="frame-item"></div>
+                        <div class="frame-child"></div>
+                        <div class="rectangle-div"></div>
+                        <div class="frame-child"></div>
+                    </div>
+                </div>
+                <div class="fa59e6781adbdced82e349bb595d99-parent">
+                    <img
+                        class="fa59e6781adbdced82e349bb595d99-icon"
+                        alt=""
+                        src="image/IMG.png"
+                    />
+
+                    <div class="frame-group">
+                        <div class="quick-reliable-warehousing-a-parent">
+                            <div class="quick-reliable-container">
+                                <span class="quick-reliable-container1">
+                                    <span class="quick-reliable-warehousing">
+                                        <span class="quick-reliable">quick & reliable </span>
+                                        <b>warehousing </b>
+                                    </span>
+                                    <b>
+                                        <span class="and">and</span>
+                                        <span class="logistics"> logistics </span>
+                                    </b>
+                                    <span class="logistics">
+                                        <span>
+                                            <span class="solution1">solution</span>
+                                            <b class="b">.</b>
+                                        </span>
+                                    </span>
+                                </span>
+                            </div>
+                            <div class="shipup-delivers-an">
+                                ShipUp delivers an unparalleled customer service through dedicated
+                                customer teams, engaged people working in an agile culture, and a
+                                global footprint
+                            </div>
+                        </div>
+                        <div class="frame-parent1">
+                            <button class="join-now-wrapper">
+                                <div class="check-price" style={{color:"#fff"}}>join now</div>
+                            </button>
+                            <div class="group-container">
+                                <img class="group-icon" alt="" src="image/Video.svg" />
+
+                                <div class="check-price">play demo</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="landing-page-inner1">
+                    <div class="frame-parent2">
+                        <div class="frame-parent3">
+                            <div class="shipup-parent">
+                                <div class="shipup">
+                                    <span class="ship">Ship</span>
+                                    <span class="up">Up</span>
+                                </div>
+                                <div class="shipup-delivers-an1">
+                                    ShipUp delivers an unparalleled customer service through
+                                    dedicated customer teams, engaged people working in an agile
+                                    culture, and a global footprint
+                                </div>
+                            </div>
+                            <div class="quick-reliable-warehousing-a-parent">
+                                <b class="enter-location">Explore</b>
+                                <div class="about-us-parent">
+                                    <div class="about-us">About Us</div>
+                                    <div class="origin">Our warehoueses</div>
+                                    <div class="about-us">Blog</div>
+                                    <div class="about-us">News and Media</div>
+                                </div>
+                            </div>
+                            <div class="legal-parent">
+                                <b class="legal">Legal</b>
+                                <div class="terms-parent">
+                                    <div class="terms">Terms</div>
+                                    <div class="privacy">Privacy</div>
+                                </div>
+                            </div>
+                            <div class="quick-reliable-warehousing-a-parent">
+                                <b class="enter-location">social media</b>
+                                <div class="social-icons">
+                                    <a href='#' >
+                                        <img
+                                            class="exodus-landing-page"
+                                            alt=""
+                                            src="image/Exodus Landing Page.svg"
+                                        />
+                                    </a>
+                                    <a href='#' >
+                                        <img
+                                            class="exodus-landing-page"
+                                            alt=""
+                                            src="image/Exodus Landing Page (1).svg"
+                                        />
+                                    </a>
+                                    <a href='#' >
+                                        <img
+                                            class="exodus-landing-page"
+                                            alt=""
+                                            src="image/Exodus Landing Page (2).svg"
+                                        />
+                                    </a>
+                                    <a href='#' >
+                                        <img
+                                            class="exodus-landing-page"
+                                            alt=""
+                                            src="image/Exodus Landing Page (3).svg"
+                                        />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="line-parent">
+                            <div class="line-div"></div>
+                            <div class="shipup">
+                                <span class="ship">Ship</span>
+                                <span class="upng">
+                                    <span class="up1">Up</span>
+                                    <span>.ng</span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="header">
+                    <div class="shipup-group">
+                        <div class="shipup1">
+                            <span class="ship">Ship</span>
+                            <span class="up2">Up</span>
+                        </div>
+                        <div class="frame-parent4">
+                            <div class="company-parent">
+                                <div class="origin">company</div>
+                                <img
+                                    class="chevron-down-icon"
+                                    alt=""
+                                    src="image/chevron-down.svg"
+                                />
+                            </div>
+                            <div class="company-parent">
+                                <div class="origin">services</div>
+                                <div class="chevron-down"></div>
+                            </div>
+                            <div class="origin">solutions</div>
+                            <div class="origin">industries</div>
+                            <div class="origin">insights</div>
+                            <div class="news-and-media1">news and media</div>
+                        </div>
+                        <div class="frame-parent5">
+                            <div class="log-in-wrapper">
+                                <div class="check-price">log in</div>
+                            </div>
+                            <div class="sign-up-wrapper">
+                                <div class="check-price">sign up</div>
+                            </div>
+                        </div>
+                        <div class="frame-parent6">
+                            <button class="request-quote-wrapper">
+                                <div class="check-price">request quote</div>
+                            </button>
+                            <button class="join-now-container">
+                                <div class="check-price">join now</div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Test
